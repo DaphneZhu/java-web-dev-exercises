91e3f233f72af142c43d4d009f4064dae2ba0c23
DaphneZhu <61849400+DaphneZhu@users.noreply.github.com>
2020-04-23 23:58:50 -0500

    add input.close()

--- src/Studio/studioCountingCharacters.java
+++ src/Studio/studioCountingCharacters.java
@@ -9,6 +9,7 @@ public class studioCountingCharacters {
         Scanner input = new Scanner(System.in); //create an input
         System.out.println("Enter the string: ");
         String quote = input.nextLine(); //input the next string
+        input.close();
 
         //String hiddenQuote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
         HashMap<Character, Integer> characterMap = new HashMap<>();
